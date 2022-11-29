d253a20ec517d14d7fe49cb52d7ec1c248190f34
Charley <charley1135@gmail.com>
2022-11-28 17:04:09 -0800

    feat:Ready to submit.

--- social-feed/src/components/DisplayPosts/DisplayPosts.jsx
+++ social-feed/src/components/DisplayPosts/DisplayPosts.jsx
@@ -10,21 +10,25 @@ const DisplayPosts = ({ post }) => {
   function handleClickLike() {
     if (thumbsUpButton === 'notliked'){
       post.isLiked = true;
+      post.isDisliked = false;
       setthumbsUpButton('thumbsup');
     }
     else {
       setthumbsUpButton('notliked');
       post.isLiked = false;
+      post.isDisliked = false;
     }
   }
 
   function handleClickDislike() {
     if (thumbsDownButton === 'notliked'){
       post.isDisliked = true;
+      post.isLiked = false;
       setthumbsDownButton('thumbsdown');
     }
     else {
       post.isDisliked = false;
+      post.isLiked = false;
       setthumbsDownButton('notliked');
     }
   }
@@ -34,8 +38,10 @@ const DisplayPosts = ({ post }) => {
       <div>
         <div className="createpostname">{post.name}</div>
         <p className="createpostpost">{post.post}</p>
+        <div className="createpostpost">
         <ThumbsUpIcon height="1.8rem" className={thumbsUpButton} onClick={handleClickLike} style={{ cursor: "pointer"}}/>
         <ThumbsDownIcon height="1.8rem" className={thumbsDownButton} onClick={handleClickDislike} style={{ cursor: "pointer"}}/>
+        </div>
       </div>
     </div>
   );
